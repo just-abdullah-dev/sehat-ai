6814bd461cb9f52e89e8dbcdce8afda7558dbd42
Abdullah <just.abdullah.dev@gmail.com>
2026-04-25 18:10:02 +0500

    only loading validator and tb model

--- Backend/app/ml/model_loader.py
+++ Backend/app/ml/model_loader.py
@@ -59,7 +59,7 @@ class ModelLoader:
         try:
             logger.info("Loading Sehat-AI ML models...")
 
-            # self._load_tb_model()
+            self._load_tb_model()
             # self._load_pneumonia_model()
             self._load_validator_model()
             # self._warmup_models()
